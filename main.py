192fb850e786d723199e63cf7a578329709981a8
Erik Fredriksson <erik.mfredriksson@gmail.com>
2017-09-16 13:44:26 +0200

    Returned the subscraper class

--- main.py
+++ main.py
@@ -1,43 +1,45 @@
 from database_manager import db_manager
+from sub_scrape import sub_scrape
 from json_loader import *
 import praw
 
-reddit_account = get_reddit()
-
-reddit = praw.Reddit(client_id=reddit_account["client_id"],
-                     client_secret=reddit_account["client_secret"],
-                     user_agent="subbreddit scraper v0.1 by /u/isThisWhatIDo")
-
-
-def get_hot(sub, limit, db, conn):
-    """Retrives the subbmissions in hot.
-
-    Sends user, title, url to a new row in the database
-    """
-    for count, submission in enumerate(reddit.subreddit(sub).top(limit=limit)):
-        if submission.author is None:
-            continue
-        else:
-            result = eval_submission(submission)
-            if result > 20:
-                title = submission.title
-                user = submission.author.name
-                url = submission.url
-                data = (title, user, url)
-                db.create_row(conn, data)
-                print("Submissions left: ", (limit - count))
+# def get_top(sub, limit, db, conn):
+#     """Retrives the subbmissions in hot.
+#
+#     Sends user, title, url to a new row in the database
+#     """
+#     for count, submission in enumerate(reddit.subreddit(sub).top(limit=limit)):
+#         if submission.author is None:
+#             continue
+#         else:
+#             result = eval_submission(submission)
+#             url = submission.url
+#             if result > 20 and is_picture(url):
+#                 title = submission.title
+#                 user = submission.author.name
+#                 data = (title, user, url)
+#                 db.create_row(conn, data)
+#                 print("Submissions left: ", (limit - count))
 
 
-def eval_submission(submission):
-    """Rate current submission.
-
-    Something for future use.
-    """
-    comments = submission.num_comments
-    ratio = submission.upvote_ratio
-    result = comments * ratio
-    return result
-
+# def is_picture(url):
+#     """Check url for common image url."""
+#     if 'imgur' or 'i.reddit' in url:
+#         return True
+#     else:
+#         return False
+#
+#
+# def eval_submission(submission):
+#     """Rate current submission.
+#
+#     Something for future use.
+#     """
+#     comments = submission.num_comments
+#     ratio = submission.upvote_ratio
+#     result = comments * ratio
+#     return result
+#
 
 def init():
     """Retrives inputs from user.
@@ -45,21 +47,24 @@ def init():
     :param subreddit: The subreddit name e.g funny
     :param db_directory: directory for database e.g /home/user/database/funn.db
     """
+    reddit_account = get_reddit()
     print("This program saves posts from a subreddit into a database")
     subreddit = input("Enter what subreddit you want to scrape /r/")
     db_directory = input("Enter directory and name for database: ")
-    limit = input("Enter amount of posts to fetch(Max: 1000): ")
-    return subreddit, db_directory, limit
+    time_filter = input("Enter time_filter(all,month,week,day,hour): ")
+    limit = int(input("Enter amount of posts to fetch(Max: 1000): "))
+    scraper = sub_scrape(subreddit, reddit_account)
+    return subreddit, db_directory, limit, scraper, time_filter
 
 
 def main():
     """Start program."""
-    subreddit, db_dir, limit = init()
+    subreddit, db_dir, limit, scraper, time_filter = init()
     db = db_manager(db_dir)
     conn = db.create_connect()
     db.create_table(conn, "top")
-    get_hot(subreddit, limit, db, conn)
+    scraper.get_posts(limit, time_filter, db, conn)
     print("Done.")
-    db.cose()
+    db.close()
 
 main()
---
+++ sub_scrape.py
@@ -0,0 +1,60 @@
+import praw
+
+
+class sub_scrape(object):
+    """Scrape a subbreddit for pictures.
+
+    Keyword arguments:
+    subreddit = string
+    limit     = integer
+
+    Then it will go to the subreddit and search through all the
+    submissions until it reaches the value of limit.
+    Outputs 2 strings containing the author(username)
+    and also the title of the post.
+    """
+
+    def __init__(self, subreddit, account):
+        """Initilize."""
+        super(sub_scrape, self).__init__()
+        self.sub = subreddit
+        self.account = account
+        self.reddit = praw.Reddit(
+            client_id=self.account["client_id"],
+            client_secret=self.account["client_secret"],
+            user_agent="subbreddit scraper v0.1 by /u/isThisWhatIDo")
+
+    def get_posts(self, limit, month, db, conn):
+        """Retrives the subbmissions in hot.
+
+        Returns user, title, url, number of comments
+        """
+        for count, submission in enumerate(self.reddit.subreddit(self.sub).top(month)):
+            if submission.author is None:
+                continue
+            else:
+                result = self.eval_submission(submission)
+                url = submission.url
+                if result > 20 and self.is_picture(url):
+                    title = submission.title
+                    user = submission.author.name
+                    data = (title, user, url)
+                    db.create_row(conn, data)
+                    print("Submissions left: ", (limit - count))
+
+    def eval_submission(self, submission):
+        """Rate current submission.
+
+        Something for future use.
+        """
+        comments = submission.num_comments
+        ratio = submission.upvote_ratio
+        result = comments * ratio
+        return result
+
+    def is_picture(self, url):
+        """Check url for common image url."""
+        if 'imgur' or 'i.reddit' in url:
+            return True
+        else:
+            return False
