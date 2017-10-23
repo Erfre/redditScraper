05c752064e81323188c8989bcaffb35627fa2583
Erik Fredriksson <erik.mfredriksson@gmail.com>
2017-10-23 09:06:56 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Reddit scraper',
+    version='0.1',
+    description='Scrapes pictures from subreddits and saves them to a database(only picture path)',
+    url='https://github.com/Erfre/reddit_scraper',
+    license='MIT',
+    install_requires=['praw', 'beautifulsoup4', 'schedule']
+)
