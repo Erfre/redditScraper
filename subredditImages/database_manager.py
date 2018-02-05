2765a788db7260a03971803c036397703a974a54
erik <laqut@hotmail.com>
2018-02-05 15:21:04 +0100

    Moving code and lceaning up used files

--- subredditImages/database_manager.py
+++ subredditImages/database_manager.py
@@ -2,6 +2,7 @@
 import random
 import sqlite3
 from sqlite3 import Error
+from random import randint
 
 
 class db_manager(object):
@@ -70,3 +71,38 @@ class db_manager(object):
         cur = conn.cursor()
         cur.execute(sql_delete)
         return cur
+
+    def count_row(self, conn):
+        """Retrieves the max id in the table"""
+        cur = conn.cursor()
+        cur.execute('SELECT max(id) FROM ' + self.table)
+        max = cur.fetchone()[0]
+        self.max_id = max
+        return
+
+    def get_random_row(self, conn):
+        """Return random row from table"""
+        cur = conn.cursor()
+
+        while True:
+            id = randint(0, self.max_id)
+            try:
+                cur.execute('SELECT * FROM ' + self.table + """ WHERE id=:rand
+                  AND reviewed=:rv""", {"rand": id, "rv": 0})
+                return cur.fetchone()
+            except:
+                print("Looks like this id doesn't exist")
+                continue
+
+    def update_desc(self, conn, id, desc):
+        """Updates the title with a description of the image."""
+        cur = conn.cursor()
+        cur.execute('UPDATE ' + self.table + ' SET title=:desc ,reviewed=:rv WHERE id=:id',
+                    {"desc": desc, "rv": "1", "id": id})
+        conn.commit()
+
+    def delete_row(self, conn, id):
+        """Deletes entry in database"""
+        cur = conn.cursor()
+        cur.execute('DELETE FROM ' + self.table + ' WHERE id=:id', {"id": id})
+        conn.commit()
